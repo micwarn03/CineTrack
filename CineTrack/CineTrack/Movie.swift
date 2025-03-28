2986905901e98e8098ea35ba798103100b57e8aa
clozinakj <cwl58@case.edu>
2025-03-28 11:13:19 -0400

    basic movie and tv show classes

---
+++ CineTrack/CineTrack/Movie.swift
@@ -0,0 +1,37 @@
+//
+//  Movie.swift
+//  CineTrack
+//
+//  Created by Chris Lozinak on 3/28/25.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+class Movie {
+    var id: Int32
+    var title: String
+//    var director: String
+    var genres: [String]
+    var year: String
+    var runtime: Int
+    var synopsis: String
+    var posterPath: String
+    var mediaType: String
+    var dateWatched: Date? = nil
+    var userReview: String? = nil
+    var thumbsUp: Bool? = nil
+    
+    init(id: Int32, title: String, /*director: String,*/ genres: [String], year: String, runtime: Int, synopsis: String, posterPath: String, mediaType: String) {
+        self.id = id
+        self.title = title
+//        self.director = director
+        self.genres = genres
+        self.year = year
+        self.runtime = runtime
+        self.synopsis = synopsis
+        self.posterPath = posterPath
+        self.mediaType = mediaType
+    }
+}
---
+++ CineTrack/CineTrack/TVShow.swift
@@ -0,0 +1,35 @@
+//
+//  TVShow.swift
+//  CineTrack
+//
+//  Created by Chris Lozinak on 3/28/25.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+class TVShow {
+    var id: Int32
+    var title: String
+    var genres: [String]
+    var years: [String]
+    var synopsis: String
+    var posterPath: String
+    var mediaType: String
+    var numSeasons: Int
+    var dateWatched: Date? = nil
+    var userReview: String? = nil
+    var thumbsUp: Bool? = nil
+    
+    init(id: Int32, title: String, genres: [String], years: [String], synopsis: String, posterPath: String, mediaType: String, numSeasons: Int) {
+        self.id = id
+        self.title = title
+        self.genres = genres
+        self.years = years
+        self.synopsis = synopsis
+        self.posterPath = posterPath
+        self.mediaType = mediaType
+        self.numSeasons = numSeasons
+    }
+}
