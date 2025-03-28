a024f816f9beff07d4c7ebe0efd3853092a04f33
Shea <96454908+AlabasterLeech@users.noreply.github.com>
2025-03-28 10:26:34 -0400

    initial creation of project files

---
+++ CineTrack/CineTrack/CineTrackApp.swift
@@ -0,0 +1,17 @@
+//
+//  CineTrackApp.swift
+//  CineTrack
+//
+//  Created by Shea Leech on 3/28/25.
+//
+
+import SwiftUI
+
+@main
+struct CineTrackApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CineTrack/CineTrack/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CineTrack
+//
+//  Created by Shea Leech on 3/28/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
