cd6c85d3cfdc2c8a7870eaa9af55856d864e7bd5
micwarn03 <micwarn03@gmail.com>
2025-03-28 11:12:46 -0400

    Views Setup

---
+++ CineTrack/CineTrack/DetailHeader.swift
@@ -0,0 +1,18 @@
+//
+//  DetailHeader.swift
+//  CineTrack
+//
+//  Created by Michael Warner on 3/28/25.
+//
+
+import SwiftUI
+
+struct DetailHeader: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    DetailHeader()
+}
